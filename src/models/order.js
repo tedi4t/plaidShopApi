c4f8a3b3393963e388873f543829e370fd0403d3
tedi4t <tedi4t@gmail.com>
2021-01-19 14:55:35 +0200

    add order model

---
+++ src/models/order.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+// Order Schema
+const orderSchema = mongoose.Schema({
+  order_id: {
+    type: Number,
+    required: true,
+    unique: true
+  },
+  color: {
+    type: Number,
+    required: true
+  },
+  size: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: String,
+    required: true
+  },
+  comment: {
+    type: String,
+    required: false
+  },
+  called: {
+    type: Boolean,
+    default: false
+  },
+  sent: {
+    type: Boolean,
+    default: false
+  },
+  received: {
+    type: Boolean,
+    default: false
+  }
+});
+
+const Order = mongoose.model('Order', orderSchema);
+
+module.exports = Order;
